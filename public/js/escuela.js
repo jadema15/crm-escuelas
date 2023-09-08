e58d81bbe1c0692ef36b9bb3a6dcd8396ff04d57
Jairo Delgado <jadema15@hotmail.com>
2023-09-08 10:43:28 -0500

    Se elimina texto inncesario dentro del select de escuela

--- public/js/escuela.js
+++ public/js/escuela.js
@@ -6,20 +6,21 @@ $(document).ready(function () {
         url: miurl
     }).done(function (resul) {
         if (resul != null) {
-          $("#escuela_id").empty();
-          for (let i = 0; i < resul.length; i++) {
-            if (resul[i].id == idX) {
-                $("#escuela_id").append("<option value='" + resul[i].id + "' selected>" + resul[i].nombre + resul[i].id + "</option>");
-            } else {
-                $("#escuela_id").append("<option value='" + resul[i].id + "'>" + resul[i].nombre +  resul[i].id+"</option>");
+            $("#escuela_id").empty();
+            for (let i = 0; i < resul.length; i++) {
+                if (resul[i].id == idX) {
+                    $("#escuela_id").append("<option value='" + resul[i].id + "' selected>" + resul[i].nombre + "</option>");
+                } else {
+                    $("#escuela_id").append("<option value='" + resul[i].id + "'>" + resul[i].nombre + "</option>");
+                }
             }
-          }
-        } 
+        }
     }).fail(function () {
         alert("Error desconocido");
     })
+    
     if ($('#error-message').length) {
-        setTimeout(function() {
+        setTimeout(function () {
             $('#error-message').addClass('hidden');
         }, 5000);
     }
